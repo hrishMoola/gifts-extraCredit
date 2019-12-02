65a2085340be9197f200bee8227678dffa29ce23
Hrishikesh Mula <hrishikeshmula@Hrishikeshs-MacBook-Pro.local>
2019-12-02 03:48:18 -0800

    Tinkering with bfq queue

--- src/main/java/Gifts.java
+++ src/main/java/Gifts.java
@@ -1,14 +1,15 @@
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Gifts {
     static Map<String, Node> allNodes = new HashMap<>();
     static Set<String> visited = new HashSet<>();
     static List<String> cycleEdges = new ArrayList<>();
-    static List<String> finalCycles = new ArrayList<>();
+    static List<String> finalAssignments = new ArrayList<>();
 
     static class Node{
         String id;
+        Integer gifts = 0;
         List<Node> listNodes;
 
         public Node(String id) {
@@ -19,6 +20,15 @@ public class Gifts {
         public Node() {
 
         }
+
+        public Integer getGifts() {
+            return gifts;
+        }
+
+        public void setGifts(Integer gifts) {
+            this.gifts += gifts;
+        }
+
 
         public String getId() {
             return id;
@@ -39,13 +49,14 @@ public class Gifts {
         @Override
         public String toString(){
             StringBuilder sb = new StringBuilder();
-            listNodes.forEach(node -> sb.append(node.getId()).append(" "));
+            listNodes.forEach(node -> sb.append(node.getId()).append(" (" + node.getGifts() +") "));
             return "Person " + id + " has friends : " + sb.toString() + " ";
         }
     }
 
     public static void main(String[] args) {
-        addPerson("1","2,4");
+//        addPerson("1","2,4");
+        addPerson("1","2,4,5,6,7");
         addPerson("2","3,4");
         addPerson("3","4");
         addPerson("4","5");
@@ -55,31 +66,60 @@ public class Gifts {
             boolean cycle = findCycles(person, "");
             System.out.println(cycle);
             if(cycle){
-                finalCycles.add(cycleEdges.toString());
+                finalAssignments.add(cycleEdges.toString());
                 cycleEdges.forEach(edge -> {
                     String friend1 = edge.split("->")[0];
                     String friend2 = edge.split("->")[1];
                     if(!friend1.equals("")){
-                    allNodes.get(friend1).getFriends().remove(allNodes.get(friend2));
-                    allNodes.get(friend2).getFriends().remove(allNodes.get(friend1));
+                        removeEdge(friend1, friend2);
                     }
                 });
             }
             visited.clear();
             cycleEdges.clear();
         }
-        System.out.println(finalCycles);
+        System.out.println(finalAssignments);
+        System.out.println("Any cycles present have been removed");
         System.out.println(allNodes.values());
+        //gonna get greedy now
+        Queue<Node> bfsQueue = new LinkedList<>();
+        List<Node> remainingNodes = new ArrayList<>(allNodes.values());
+        bfsQueue.offer(remainingNodes.get(0));
+        System.out.println("bfsQueue = " + bfsQueue);
+        while(!bfsQueue.isEmpty()){
+            System.out.println("entered");
+            Node person = bfsQueue.poll();
+            List<Node> friends = person.getFriends();
+            List<String> assignments = new ArrayList<>();
+            for (Node friend : friends) {
+                if (person.getGifts() == 0 || person.getGifts() == -1) {
+                    assignments.add(person + "->" + friend.getId());
+                    person.setGifts(1);
+                    friend.setGifts(-1);
+                } else if (person.getGifts() == 1) {
+                    assignments.add(friend.getId() + "->" + person);
+                    person.setGifts(-1);
+                    friend.setGifts(1);
+                }
+                bfsQueue.offer(friend);
+            }
+            finalAssignments.add(assignments.toString());
+        };
+        System.out.println(finalAssignments);
+        System.out.println(remainingNodes);
+
+//        Boolean assignmentPossible = assign();
+
     }
 
     private static void removeEdge(String friend1, String friend2) {
-
+        allNodes.get(friend1).getFriends().remove(allNodes.get(friend2));
+        allNodes.get(friend2).getFriends().remove(allNodes.get(friend1));
     }
 
     private static boolean findCycles(String person, String parent) {
         visited.add(person);
-        if(person.length() > 0)
-            cycleEdges.add(parent + "->" +person);
+        cycleEdges.add(parent + "->" +person);
         for(Node friend: allNodes.get(person).getFriends()){
             if(!visited.contains(friend.getId())){
                 if(findCycles(friend.getId(), person)){
@@ -87,11 +127,12 @@ public class Gifts {
                 }
             } else if(!friend.getId().equalsIgnoreCase(parent)){
                 {
-                    cycleEdges.add(person + "->" + friend.getId());
+                cycleEdges.add(person + "->" + friend.getId());
                 return true;
                 }
             }
         }
+        cycleEdges.remove(parent + "->" +person);
         return false;
     }
 
@@ -100,7 +141,8 @@ public class Gifts {
             allNodes.put(person, new Node(person));
         }
 
-        for (String friend : friends.split(",")) {
+        for (String fatFriend : friends.split(",")) {
+            String friend = fatFriend.trim();
             if(!allNodes.containsKey(friend)){
                 allNodes.put(friend, new Node(friend));
             }
